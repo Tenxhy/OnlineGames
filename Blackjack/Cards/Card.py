d12b08c61eb8fb21cbd2e8de7a1979bcd128d4b7
Matins7 <riccardo.matino@iisagnoletti.it>
2024-05-07 10:07:00 +0200

    Blackjack:
    - Creating the file Displays.yml
    - Adding the function dislpay suit in Suit
    - Adding the function get_display in Card

--- Blackjack/Cards/Card.py
+++ Blackjack/Cards/Card.py
@@ -1,28 +1,45 @@
 from .Suit import Suit
 from .Value import Value
+import os, yaml
+
+display_ranks = []
+
+with open(os.path.dirname(__file__) + r'\Displays.yml', 'r') as file:
+    openedFile = yaml.safe_load(file)
+    display_ranks = openedFile["cards"]["ranks"]
 
 class Card():
     '''A class used to represent a game card'''
     def __init__(self, value: Value, suit: Suit, isHidden: bool = False):
-        self._value = value
-        self._suit = suit
-        self._isHidden = isHidden
+        self.__value = value
+        self.__suit = suit
+        self.__isHidden = isHidden
     
     def getValue(self):
-        return self._value
+        return self.__value
     
     def getSuit(self):
-        return self._suit
+        return self.__suit
 
     def getFormattedName(self):
         return str(self.getValue().name) + " OF " + str(self.getSuit().name)
     
     def isHidden(self):
-        return self._isHidden
+        return self.__isHidden
     
     def setHiddenStatus(self, hidden_status: bool):
-        self._isHidden = hidden_status
+        self.__isHidden = hidden_status
     
+    def get_display(self) -> list:
+        value = str(self.getValue().value)
+        if value == 10:
+            if self.getValue().name.lower() in display_ranks.keys():
+                value = display_ranks[self.getValue().name.lower()]
+
+        [str(line).replace("X", value) for line in self.getSuit().get_display()]
+
+    def display(self) -> None:
+        [print(line) for line in self.get_display()]
+
     def __str__(self) -> str:
         return self.getFormattedName()
-    
--- Blackjack/Cards/Suit.py
+++ Blackjack/Cards/Suit.py
@@ -1,4 +1,13 @@
 from enum import Enum, auto
+import os, yaml
+
+display_card = []
+display_symbols = []
+
+with open(os.path.dirname(__file__) + r'\Displays.yml', 'r') as file:
+    openedFile = yaml.safe_load(file)
+    display_card = openedFile["cards"]["display"]
+    display_symbols = openedFile["cards"]["symbols"]
 
 class Suit(Enum):
 
@@ -6,4 +15,20 @@ class Suit(Enum):
     DIAMONDS = auto()
     CLUBS = auto()
     HEARTS = auto()
-    
+
+    def get_display(self) -> list:
+        symbol = "Y"
+        if self == Suit.SPADES:
+            symbol = display_symbols[self.name.lower()]
+        elif self == Suit.DIAMONDS:
+            symbol = display_symbols[self.name.lower()]
+        elif self == Suit.CLUBS:
+            symbol = display_symbols[self.name.lower()]
+        elif self == Suit.HEARTS:
+            symbol = display_symbols[self.name.lower()]
+        
+        displayCard = [str(line).replace("Y", symbol) for line in display_card]
+        return displayCard
+
+print(Suit.SPADES.get_display())
+
